c4329496c8d8e0a39e5d010a17cd611dbaa83a8a
SIGMA-L <67269568+SIGMA-L@users.noreply.github.com>
2023-08-07 16:31:42 +0900

    Upload File

---
+++ assets/js/script.js
@@ -0,0 +1,32 @@
+// console.log('--- ' + Date().split(' ')[4] + ' ---');
+
+window.onload = function(){
+	getJSON();
+	let elemEventsContent = '';
+	let elemEvents = $('events');
+	// console.log(elemEvents);
+	for(let i = 0; i < events.length; i++){
+		let content = '<dl><dt>'+ events[i].t +'</dt><dd><span style="color:'+ events[i].cc[0] +';background:'+ events[i].cc[1] +'">'+ events[i].c +'</span></dd><dd>'+ events[i].d +'</dd></dl>';
+		elemEventsContent += content;
+	}
+	elemEvents.innerHTML = elemEventsContent;
+}
+
+function getJSON(){
+	let req = new XMLHttpRequest();
+	req.onreadystatechange = function(){
+		if(req.readyState == 4 && req.status == 200){
+			// console.log('読み込んだよ');
+			events = JSON.parse(req.responseText);
+			// console.log(events);
+		}
+	};
+	//下のファイルパスは仮のものであるため、後に正規の場所に移行すること。また、ローカルファイルはブラウザの同一生成元ポリシーにより受け付けられない。
+	req.open("GET", "../../data/event.json", false);
+	// req.open("GET", "", false);
+	req.send(null);
+}
+
+function $(elemId){
+	return document.getElementById(elemId);
+}
